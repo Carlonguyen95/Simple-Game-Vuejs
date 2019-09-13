4697ba83f03a590ae317622a58b98b36af0ffcc4
carlonguyen <carlohoa@gmail.com>
2019-09-13 12:57:40 +0200

    Added initial health constant

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 new Vue({
   el: '#app',
   data: {
+    initialHealth: 100,
+    gameIsRunning: false,
+    gameAnnounceTextList: [],
+
     player: {
       racial: 'human',
-      health: 100,
+      health: this.initialHealth,
       alive: true,
+      special: true
     },
     mob: {
       racial: 'monster',
-      health: 100,
+      health: this.initialHealth,
       alive: true,
-    },
-    gameIsRunning: false,
-    gameAnnounceTextList: []
+      special: false
+    }
   },
   methods: {
     startGame: function () {
       this.gameIsRunning = true;
-      this.player.health = this.mob.health = 100;
+      this.player.health = this.mob.health = this.initialHealth;
       this.gameAnnounceTextList = [];
     },
     startNewGame: function () {
-      if (confirm('New game?')) {
+      if (confirm('Start a new game?')) {
         this.startGame();
       } else {
         this.gameIsRunning = false;
@@ -29,61 +33,95 @@ new Vue({
     },
 
     attack: function () {
-      // if(this.checkWin()){
-      //   return;
-      // }
-      let damagePlayer;
-      let damageMonster;
-      this.player.health -= damagePlayer = this.generateDamage(this.player.racial);
-      this.mob.health -= damageMonster = this.generateDamage(this.mob.racial);
-
-      this.gameAnnounceTextList.push('Monster attacked for ' + damagePlayer);
-      this.gameAnnounceTextList.push('You attacked for ' + damageMonster);
+      this.monsterAttack();
+      this.playerAttack();
       this.checkWin();
     },
     attackSpecial: function () {
-      
+      this.monsterAttack();
+      this.playerAttack(true);
+      this.checkWin();
     },
-
+    monsterAttack: function () {
+      let damage;
+      this.player.health -= damage = this.generateDamage(this.mob.racial);
+      if(this.player.health < 0){
+        this.player.health = 0;
+      }
+      this.gameAnnounceUpdate("", "", damage);
+    },
+    playerAttack: function (special) {
+      let damage;
+      if(special){
+        this.mob.health -= damage = this.generateDamage(this.mob.racial, this.player.special);
+      }else{
+        this.mob.health -= damage = this.generateDamage(this.mob.racial);
+      }
+      if(this.mob.health < 0){
+        this.mob.health = 0;
+      }
+      this.gameAnnounceUpdate("", damage, "");
+    },
     heal: function () {
-      
+      let heal;
+      heal = Math.max(Math.floor(Math.random() * 40) + 1, 20);
+      this.player.health += heal;
+      this.gameAnnounceUpdate(heal, "", "");
+      this.monsterAttack();
+      this.checkWin();
     },
     
     giveUp: function () {
       this.gameIsRunning = false;
-      this.player.health = this.mob.health = 100;
+      this.player.health = this.mob.health = this.initialHealth;
       this.gameAnnounceTextList = [];
     },
-    
-    generateDamage: function (racial) {
+
+    generateDamage: function (racial, special) {
       let damageMax = 10;
       let damageMin = 3;
       let damage = 0;
 
-      if (racial == 'human'){
-        damageMax = 20;
-        damage = Math.max(Math.floor(Math.random() * damageMax) + 1, damageMin);
+      if (racial === 'monster') {
+        damageMax = 25;
+        damageMin = 10;
+        return damage = Math.max(Math.floor(Math.random() * damageMax) + 1, damageMin);
       } else {
-        damage = Math.max(Math.floor(Math.random() * damageMax) + 1, damageMin);
+        return damage = Math.max(Math.floor(Math.random() * damageMax) + 1, damageMin);
+      }
+
+      if (special) {
+        damageMax = 30;
+        return damage = Math.max(Math.floor(Math.random() * damageMax) + 1, 20);
       }
 
       return damage;
     },
 
     checkWin: function () {
-      if (this.player.health <= 0 || this.mob.health <= 0) {
+      if (this.player.health <= 0) {
         this.gameIsRunning = false;
-        if (this.player.health <= 0 ? this.mob.alive = true : this.player.alive = false) {
-          this.gameAnnounceTextList.push('You lost!');
-          this.startNewGame();
-          return true;
-        } else {
-          this.gameAnnounceTextList.push('You won the big battle!');
-          this.startNewGame();
-          return true;
-        }
+        this.player.alive = false;
+        this.gameAnnounceTextList.push('You have been defeated!');
+        // this.startNewGame();
       }
-      return false;
+      if (this.mob.health <= 0) {
+        this.mob.alive = false;
+        this.gameIsRunning = false;
+        this.gameAnnounceTextList.push('You won the battle!');
+        // this.startNewGame();
+      }
+    },
+    gameAnnounceUpdate: function (heal, playerAttack, monsterAttack) {
+      if(heal.length != 0){
+        this.gameAnnounceTextList.push('You healed for ' + heal + ' health!');
+      }
+      if(playerAttack.length != 0){
+        this.gameAnnounceTextList.push('You hit for ' + playerAttack + ' damage!');
+      }
+      if(monsterAttack.length != 0){
+        this.gameAnnounceTextList.push('Monster attacked for ' + monsterAttack + ' damage!');
+      }
     }
   }
 })
